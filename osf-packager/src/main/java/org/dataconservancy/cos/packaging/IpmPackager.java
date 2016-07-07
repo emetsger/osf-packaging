39ae86ac327b068b8fe302113e0580ab01cc4c0b
Elliot Metsger <emetsger@jhu.edu>
2016-07-07 10:07:03 -0400

    [WIP] Spring-ifying the IpmPackager and other work.

--- osf-packager/src/main/java/org/dataconservancy/cos/packaging/IpmPackager.java
+++ osf-packager/src/main/java/org/dataconservancy/cos/packaging/IpmPackager.java
@@ -15,12 +15,24 @@
  */
 package org.dataconservancy.cos.packaging;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.ResourceFactory;
 import org.apache.jena.riot.RDFFormat;
+import org.dataconservancy.cos.osf.client.model.Registration;
+import org.dataconservancy.cos.osf.client.model.User;
+import org.dataconservancy.cos.osf.client.service.OsfService;
 import org.dataconservancy.cos.osf.packaging.OsfPackageGraph;
+import org.dataconservancy.cos.rdf.support.OwlClasses;
+import org.dataconservancy.cos.rdf.support.OwlProperties;
+import org.dataconservancy.cos.rdf.support.Rdf;
 import org.dataconservancy.packaging.tool.api.Package;
 import org.dataconservancy.packaging.tool.api.PackageGenerationService;
 import org.dataconservancy.packaging.tool.impl.IpmRdfTransformService;
@@ -31,15 +43,19 @@ import org.dataconservancy.packaging.tool.model.PropertiesConfigurationParameter
 import org.dataconservancy.packaging.tool.model.RDFTransformException;
 import org.dataconservancy.packaging.tool.model.ipm.FileInfo;
 import org.dataconservancy.packaging.tool.model.ipm.Node;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import javax.swing.text.html.HTMLDocument;
 import java.io.ByteArrayInputStream;
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 /**
  * Created by esm on 6/22/16.
@@ -48,47 +64,59 @@ import java.util.UUID;
  * @author esm
  */
 public class IpmPackager {
+
+    static final String WB_HOST = "192.168.99.100";
+
+    static final Logger LOG = LoggerFactory.getLogger(IpmPackager.class);
+
     static final String PACKAGE_NAME = "MyPackage";
 
     static final ClassPathXmlApplicationContext cxt =
             new ClassPathXmlApplicationContext("classpath*:applicationContext.xml",
                     "classpath*:org/dataconservancy/config/applicationContext.xml",
-                    "classpath*:org/dataconservancy/packaging/tool/ser/config/applicationContext.xml");
+                    "classpath*:org/dataconservancy/packaging/tool/ser/config/applicationContext.xml",
+                    "classpath*:org/dataconservancy/cos/osf/client/config/applicationContext.xml",
+                    "classpath:/org/dataconservancy/cos/packaging/config/applicationContext.xml");
+
+    private static final Property OSF_FILE_NAME = ResourceFactory.createProperty(OwlProperties.OSF_HAS_NAME.fqname());
+
+    private static final Property OSF_BINARY_URI =
+            ResourceFactory.createProperty(OwlProperties.OSF_HAS_BINARYURI.fqname());
+
+    private static final Resource OSF_FILE = ResourceFactory.createResource(OwlClasses.OSF_FILE.fqname());
+
+    private static final Property OSF_HAS_NODE = ResourceFactory.createProperty(OwlProperties.OSF_HAS_NODE.fqname());
+
+    private static final Property OSF_PROVIDER_NAME =
+            ResourceFactory.createProperty(OwlProperties.OSF_PROVIDER_NAME.fqname());
+
+    private static final Property RDF_TYPE = ResourceFactory.createProperty(Rdf.Ns.RDF, "type");
 
     public static void main(String[] args) throws Exception {
 
-        IpmRdfTransformService ipm2rdf =
-                cxt.getBean(IpmRdfTransformService.class);
+        final OsfPackageGraph packageGraph = cxt.getBean("packageGraph", OsfPackageGraph.class);
+        final OsfService osfService = cxt.getBean("osfService", OsfService.class);
+        final String registrationId = "w4qxy";
 
-        PackageState state = new PackageState();
+        final Registration registration = osfService.registration(registrationId).execute().body();
+        final List<User> users = registration.getContributors().stream()
+                .map(c -> {
+                    try {
+                        return osfService.user(c.getId()).execute().body();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e.getMessage(), e);
+                    }
+                })
+                .collect(Collectors.toList());
 
-        /* Put the domain object RDF into the package state */
-        state.setDomainObjectRDF(ModelFactory.createDefaultModel()
-                .read(IpmPackager.class
-                                .getResourceAsStream("/content/domainObjects.ttl"),
-                        null,
-                        "TTL"));
+        packageGraph.add(registration);
+        users.forEach(packageGraph::add);
 
-        /*
-         * Now put the IPM tree in the package state. We build it in java via
-         * buildContentTree(), then serialize to RDF
-         */
-        state.setPackageTree(ipm2rdf.transformToRDF(buildContentTree()));
-
-        /* Construct the package */
-        Package pkg = buildPackage(state);
-
-        /* Now just write the package out to a file */
-        FileOutputStream out = new FileOutputStream(PACKAGE_NAME + ".tar.gz");
-        IOUtils.copy(pkg.serialize(), out);
-        out.close();
-        pkg.cleanupPackage();
-
-        System.out.println("DONE");
+        buildPackage(packageGraph);
 
     }
 
-    public static void build(OsfPackageGraph graph) {
+    public static void buildPackage(OsfPackageGraph graph) {
 
         IpmRdfTransformService ipm2rdf =
                 cxt.getBean(IpmRdfTransformService.class);
@@ -97,6 +125,12 @@ public class IpmPackager {
 
         ByteArrayOutputStream sink = new ByteArrayOutputStream();
         graph.serialize(sink, RDFFormat.TURTLE_PRETTY, graph.OSF_SELECTOR);
+
+        try {
+            LOG.debug("Packaging graph:\n{}", IOUtils.toString(sink.toByteArray(), "UTF-8"));
+        } catch (IOException e) {
+            // ignore
+        }
 
         /* Put the domain object RDF into the package state */
         state.setDomainObjectRDF(ModelFactory.createDefaultModel()
@@ -136,7 +170,14 @@ public class IpmPackager {
 
         System.out.println("DONE");
     }
-
+    
+    /*
+     * We manually build the IPM tree here. Fundamentally, we're doing three
+     * things: 1) Creating "directory" nodes that correspond to a domain object.
+     * 2) Creating "content" nodes that correspond to a domain object that
+     * describes associated content. 3) Arranging these nodes into a tree
+     * structure of our liking.
+     */
     private static Node buildContentTree(Model domainObjects) {
         // Synthesize a root node to anchor the objects in the domain object graph
         Node root = new Node(URI.create(UUID.randomUUID().toString()));
@@ -148,73 +189,52 @@ public class IpmPackager {
         //   - Otherwise, make a directory node
 
         domainObjects.listSubjects().forEachRemaining(subject -> {
-            if (!subject.isAnon()) {
-                URI u = URI.create(subject.getURI());
 
-                // Hash URIs do not get their own node; they will be considered to be a single node.
-                if (u.getFragment() != null) {
-                    return;
-                }
+            if (subject.isAnon()) {
+                LOG.debug("Skipping IPM node creation for anonymous resource '{}'", subject.getId().toString());
+                return;
+            }
 
-                String fileName;
+            URI u = URI.create(subject.getURI());
+
+            // Hash URIs do not get their own node; they will be considered to be a single node.
+            if (u.getFragment() != null) {
+                LOG.debug("Skipping IPM node creation for hash URI resource '{}'", subject.getURI());
+                return;
+            }
+
+            String msgFmt = "Creating %s IPM node named %s for domain object %s";
+
+            Node n = new Node(u);
+            n.setDomainObject(u);
+
+            if (isFile(subject, domainObjects)) {
+                String binaryUri = getBinaryUri(subject, domainObjects);
+                String filename = getFileName(subject, domainObjects);
+                LOG.info(String.format(msgFmt, "binary file", filename, subject.getURI()));
+
+                n.setFileInfo(
+                        contentFromUrl(
+                                filename,
+                                binaryUri));
+
+            } else {
+                String filename;
                 if (u.getPath() != null) {
                     String[] pathElements = u.getPath().split("\\/");
-                    fileName = escape(pathElements[pathElements.length - 1]);
+                    filename = escape(pathElements[pathElements.length - 1]);
                 } else {
-                    fileName = escape(subject.getURI());
+                    filename = escape(subject.getURI());
                 }
-
-                System.err.println("Creating node for subject " + subject + " with file name " + fileName);
-                Node n = new Node(u);
-                n.setDomainObject(u);
-                n.setFileInfo(directory(fileName));
-                root.addChild(n);
+                LOG.info(String.format(msgFmt, "directory", filename, subject.getURI()));
+                n.setFileInfo(directory(filename));
             }
+
+            root.addChild(n);
+
         });
 
         return root;
-    }
-
-    /*
-     * We manually build the IPM tree here. Fundamentally, we're doing three
-     * things: 1) Creating "directory" nodes that correspond to a domain object.
-     * 2) Creating "content" nodes that correspond to a domain object that
-     * describes associated content. 3) Arranging these nodes into a tree
-     * structure of our liking.
-     */
-    private static Node buildContentTree() throws Exception {
-
-        /*
-         * First the nodes - correlate each node with a domain object and
-         * possibly content
-         */
-
-        /*
-         * These are the IDs of our domain objects. In this quick example, we
-         * know this a priori
-         */
-        final String id1 = "test:/1";
-        final String id2 = "test:/2";
-        final String id3 = "test:/3";
-
-        Node one = new Node(URI.create(id1));
-        one.setDomainObject(URI.create(id1));
-        one.setFileInfo(directory("one"));
-
-        Node two = new Node(URI.create(id2));
-        two.setDomainObject(URI.create(id2));
-        two.setFileInfo(directory("two"));
-
-        Node three = new Node(URI.create(id3));
-        three.setDomainObject(URI.create(id3));
-        three.setFileInfo(contentFromClasspath("/content/three.txt"));
-
-        /* Now the hierarchy */
-
-        one.addChild(two);
-        two.addChild(three);
-
-        return one;
     }
 
     /**
@@ -232,6 +252,52 @@ public class IpmPackager {
     private static FileInfo contentFromClasspath(String path) throws Exception {
         FileInfo info = new FileInfo(Paths
                 .get(IpmPackager.class.getResource(path).toURI()));
+        info.setIsFile(true);
+        return info;
+    }
+
+    /**
+     * Create a FileInfo that points to file content present at a URL.  The content from the URL is downloaded and
+     * stored in a temporary file.
+     * <p>
+     * The logical name of the file represented in the FileInfo is the {@code name} parameter.
+     * </p>
+     *
+     * @param filename the logical name of the content represented by the returned {@code FileInfo}
+     * @param contentUrl resolvable URL to the content
+     * @return populated FileInfo
+     * @throws RuntimeException if the content cannot be downloaded or saved to a temporary file
+     */
+    private static FileInfo contentFromUrl(String filename, String contentUrl) {
+
+        if (contentUrl.contains("localhost") && WB_HOST != null) {
+            contentUrl = contentUrl.replace("localhost", WB_HOST);
+        }
+
+        LOG.debug("  Retrieving '{}' content from '{}'", filename, contentUrl);
+
+        Request request = new Request.Builder()
+                .get()
+                .url(contentUrl)
+                .build();
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        OkHttpClient client = builder.build();
+        File outFile = null;
+        try {
+            Response response = client.newCall(request).execute();
+
+            if (!response.isSuccessful()) {
+                throw new RuntimeException("Unable to retrieve content from " + contentUrl + ": " + response.code() + " - " + response.message());
+            }
+
+            outFile = new File(System.getProperty("java.io.tmpdir"), filename);
+            IOUtils.copy(response.body().byteStream(),
+                    new FileOutputStream(outFile));
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+
+        FileInfo info = new FileInfo(outFile.toPath());
         info.setIsFile(true);
         return info;
     }
@@ -278,7 +344,77 @@ public class IpmPackager {
         return generator.generatePackage(state, params);
     }
 
+    /**
+     * Escapes troublesome characters from file names.
+     * 
+     * @param candidateFilename a candidate filename that may contain characters to be escaped
+     * @return the escaped filename
+     */
     private static String escape(String candidateFilename) {
-        return candidateFilename.replace(":", "_");
+        return candidateFilename
+                .replace(":", "_")
+                .replace(" ", "_");
     }
+
+    /**
+     * Examines the RDF type of the resource which represents a subject in the supplied {@code domainObjects} model,
+     * and returns {@code true} if it is an {@code osf:File}.  Objects that have an {@code rdf:type} of {@code osf:File}
+     * are expected to have the following predicates present in {@code domainObjects}:
+     * <pre>
+     * &lt;subject&gt; a osf:File                              .
+     * &lt;subject&gt;   osf:hasName      "some file name"     .
+     * &lt;subject&gt;   osf:providerName "file provider name" .
+     * &lt;subject&gt;   osf:hasBinaryUri "&lt;url to content&gt;"   .
+     * </pre>
+     *
+     * @param subject a resource from the supplied {@code domainObjects} which may be an {@code osf:File}
+     * @param domainObjects Jena model containing the triples for all the domain objects in a package
+     * @return true if the {@code subject} is a {@code osf:File}
+     */
+    private static boolean isFile(Resource subject, Model domainObjects) {
+        return domainObjects.contains(subject, RDF_TYPE, OSF_FILE);
+
+    }
+
+    /**
+     * Uses the values of the {@code osf:hasName} and {@code osf:providerName} predicates to generate a logical filename
+     * for the file represented by the domain object with the supplied {@code subject}. Objects that have an
+     * {@code rdf:type} of {@code osf:File} are expected to have the following predicates present in
+     * {@code domainObjects}:
+     * <pre>
+     * &lt;subject&gt; a osf:File                              .
+     * &lt;subject&gt;   osf:hasName      "some file name"     .
+     * &lt;subject&gt;   osf:providerName "file provider name" .
+     * &lt;subject&gt;   osf:hasBinaryUri "&lt;url to content&gt;"   .
+     * </pre>
+     *
+     * @param subject a resource from the supplied {@code domainObjects} which is an {@code osf:File}
+     * @param domainObjects Jena model containing the triples for all the domain objects in a package
+     * @return a logical filename for the supplied {@code subject}
+     */
+    private static String getFileName(Resource subject, Model domainObjects) {
+        String baseName = escape(domainObjects.getProperty(subject, OSF_FILE_NAME).getObject().toString());
+        String providerName = domainObjects.getProperty(subject, OSF_PROVIDER_NAME).getObject().toString();
+        return escape(providerName + "_" + baseName);
+    }
+
+    /**
+     * Obtains the value of the {@code osf:hasBinaryUri} predicate from {@code domainObjects} for the supplied
+     * {@code subject}.  Objects that have an {@code rdf:type} of {@code osf:File} are expected to have the following
+     * predicates present in {@code domainObjects}:
+     * <pre>
+     * &lt;subject&gt; a osf:File                              .
+     * &lt;subject&gt;   osf:hasName      "some file name"     .
+     * &lt;subject&gt;   osf:providerName "file provider name" .
+     * &lt;subject&gt;   osf:hasBinaryUri "&lt;url to content&gt;"   .
+     * </pre>
+     *
+     * @param subject a resource from the supplied {@code domainObjects} which is an {@code osf:File}
+     * @param domainObjects Jena model containing the triples for all the domain objects in a package
+     * @return the value of the {@code osf:hasBinaryUri} predicate for the supplied {@code subject}
+     */
+    private static String getBinaryUri(Resource subject, Model domainObjects) {
+        return domainObjects.getProperty(subject, OSF_BINARY_URI).getObject().asLiteral().getString();
+    }
+
 }
